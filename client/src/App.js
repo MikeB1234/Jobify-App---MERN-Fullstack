1c70500cea9de8e1374bdda4648adef7927978bd
MikeB1234 <mbacha2345@gmail.com>
2022-03-31 15:34:56 -0400

    created landing page component

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
+import Landing from './pages/Landing'
 
 function App() {
   return (
-    <h1>Jobify App</h1>
+    <>
+      <h1>Jobify App</h1>
+      <Landing />
+    </>
   )
 
 }
---
+++ client/src/pages/Landing.js
@@ -0,0 +1,28 @@
+import logo from '../assets/images/logo.svg'
+import main from '../assets/images/main.svg'
+
+const Landing = () => {
+    return (
+        <main>
+            <nav>
+                <img src={logo} alt='jobify' className='logo' />
+            </nav>
+            <div className="container page">
+                <div className='info'>
+                    <h1>
+                        job <span>tracking</span> app
+                    </h1>
+                    <p>
+                        lorem ipsum
+                    </p>
+                    <button className="btn btn-hero">
+                        Login/Register
+                    </button>
+                </div>
+                <img src={main} alt="job hunt" className="img main-img" />
+            </div>
+        </main>
+    )
+}
+
+export default Landing
