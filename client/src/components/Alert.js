0b7d672cc78365c2fd508a71eef964ac564180fe
MikeB1234 <mbacha2345@gmail.com>
2022-04-01 15:24:04 -0400

    created Register page with Form & Alert components

---
+++ client/src/components/Alert.js
@@ -0,0 +1,8 @@
+
+const Alert = () => (
+    <div className="alert alert-danger">
+        Alert Goes Here
+    </div>
+)
+
+export default Alert
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -3,28 +3,26 @@ import Wrapper from '../assets/wrappers/LandingPage'
 import { Logo } from '../components/index'
 import { Link } from 'react-router-dom'
 
-const Landing = () => {
-    return (
-        <Wrapper>
-            <nav>
-                <Logo />
-            </nav>
-            <div className="container page">
-                <div className='info'>
-                    <h1>
-                        job <span>tracking</span> app
-                    </h1>
-                    <p>
-                        lorem ipsum
-                    </p>
-                    <Link to='/register' className="btn btn-hero">
-                        Login/Register
-                    </Link>
-                </div>
-                <img src={main} alt="job hunt" className="img main-img" />
+const Landing = () => (
+    <Wrapper>
+        <nav>
+            <Logo />
+        </nav>
+        <div className="container page">
+            <div className='info'>
+                <h1>
+                    job <span>tracking</span> app
+                </h1>
+                <p>
+                    lorem ipsum
+                </p>
+                <Link to='/register' className="btn btn-hero">
+                    Login/Register
+                </Link>
             </div>
-        </Wrapper>
-    )
-}
+            <img src={main} alt="job hunt" className="img main-img" />
+        </div>
+    </Wrapper>
+)
 
 export default Landing
