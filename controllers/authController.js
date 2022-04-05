fb756d2050bdfb573e7131fcd1feac9e67ffce10
MikeB1234 <mbacha2345@gmail.com>
2022-04-05 18:55:57 -0400

    created notFound and badRequest error subsclasses and refactored

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,17 +1,15 @@
 import User from '../models/User.js'
 import { StatusCodes } from 'http-status-codes'
-
-class CustomAPIError extends Error {
-    constructor(message) {
-        super(message)
-        this.statusCode = StatusCodes.BAD_REQUEST
-    }
-}
+import { BadRequestError } from '../errors/index.js'
 
 const register = async (req, res) => {
-    const { name, email, password } = req.body
-    if (!name || !email || !password) {
-        throw new CustomAPIError('Please provide all values')
+    const { firstName, email, password } = req.body
+    if (!firstName || !email || !password) {
+        throw new BadRequestError('Please provide all values')
+    }
+    const userAlreadyExists = await User.findOne({ email })
+    if (userAlreadyExists) {
+        throw new BadRequestError('Email already in use')
     }
     const user = await User.create({ name, email, password })
     res.status(StatusCodes.OK).json({ user })
