c3bdd5e34d459bc877ea69436a787eb0ec18b4e9
MikeB1234 <mbacha2345@gmail.com>
2022-04-14 17:10:40 -0400

    setup nested pages structure for Dashboard with React Route

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,25 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
-import { Error, Register, Dashboard, Landing } from './pages/index'
+import { Error, Register, Landing } from './pages/index'
+import { AddJob, AllJobs, Profile, SharedLayout, Stats } from './pages/dashboard/index'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Dashboard />} />
+
+        <Route path='/' element={<SharedLayout />}>
+          <Route index element={<Stats />} />
+          <Route path='all-jobs' element={<AllJobs />} />
+          <Route path='add-job' element={<AddJob />} />
+          <Route path='profile' element={<Profile />} />
+        </Route>
+
         <Route path='/register' element={<Register />} />
         <Route path='/landing' element={<Landing />} />
         <Route path='*' element={<Error />} />
+
       </Routes>
     </BrowserRouter>
   )
 }
-
 export default App;
