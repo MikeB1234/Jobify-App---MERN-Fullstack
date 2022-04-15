16738bfdfd4416ce831bcd6bb02c0907c3851790
MikeB1234 <mbacha2345@gmail.com>
2022-04-15 12:24:45 -0400

    toggle logout dropdown button in local state

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,9 +2,11 @@ import Wrapper from "../assets/wrappers/Navbar";
 import { FaAlignLeft, FaUserCircle, FaCaretDown } from 'react-icons/fa'
 import { useAppContext } from "../context/appContext";
 import Logo from "./Logo";
+import { useState } from "react";
 
 const Navbar = () => {
     const { toggleSidebar } = useAppContext()
+    const [showLogout, setShowLogout] = useState(false)
     return (
         <Wrapper>
             <div type='button' className="nav-center">
@@ -16,19 +18,19 @@ const Navbar = () => {
                     <h3 className="logo-text">Dashboard</h3>
                 </div>
                 <div className="btn-container">
-                    <button type='button' className="btn" onClick={() => console.log('Show/Hide')}>
+                    <button type='button' className="btn" onClick={() => setShowLogout(!showLogout)}>
                         <FaUserCircle />
                         Mike
                         <FaCaretDown />
                     </button>
-                    <div className="dropdown show-dropdown">
-                        <button type='button' className="dropdown-btn" onClick={() => console.log('Logout')}>
+                    <div className={showLogout ? 'dropdown show-dropdown' : 'dropdown'} >
+                        <button type='button' className='dropdown-btn' onClick={() => console.log('logout')}>
                             Logout
                         </button>
                     </div>
                 </div>
             </div>
-        </Wrapper>
+        </Wrapper >
     )
 }
 
