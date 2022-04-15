a0440b7810998f6f8a3f19a7f2d201bf35c2d16b
MikeB1234 <mbacha2345@gmail.com>
2022-04-15 14:36:44 -0400

    big sidebar component complete

--- client/src/components/BigSidebar.js
+++ client/src/components/BigSidebar.js
@@ -1,9 +1,20 @@
 import Wrapper from "../assets/wrappers/BigSidebar"
+import { useAppContext } from '../context/appContext'
+import Logo from './Logo'
+import NavLinks from './NavLinks'
 
 const BigSidebar = () => {
+    const { showSidebar } = useAppContext()
     return (
         <Wrapper>
-            BigSidebar
+            <div className={showSidebar ? 'sidebar-container show-sidebar' : 'sidebar-container'}>
+                <div className='content'>
+                    <header>
+                        <Logo />
+                    </header>
+                    <NavLinks />
+                </div>
+            </div>
         </Wrapper>
     )
 }
