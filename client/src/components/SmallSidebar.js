fdf701d8387beef52088f73a2b84046f8a51a81a
MikeB1234 <mbacha2345@gmail.com>
2022-04-15 13:37:26 -0400

    setup small sidebar component structure

--- client/src/components/SmallSidebar.js
+++ client/src/components/SmallSidebar.js
@@ -1,9 +1,26 @@
-import Wrapper from "../assets/wrappers/SmallSidebar"
+import Wrapper from '../assets/wrappers/SmallSidebar'
+import { FaTimes } from 'react-icons/fa'
+import { useAppContext } from '../context/appContext'
+import Logo from './Logo'
+import { NavLink } from 'react-router-dom'
+import links from '../utils/links'
 
 const SmallSidebar = () => {
     return (
         <Wrapper>
-            SmallSidebar
+            <div className='sidebar-container show-sidebar'>
+                <div className='content'>
+                    <button className='close-btn' onClick={() => console.log('toggle')}>
+                        <FaTimes />
+                    </button>
+                    <header>
+                        <Logo />
+                    </header>
+                    <div className='nav-links'>
+                        links
+                    </div>
+                </div>
+            </div>
         </Wrapper>
     )
 }
